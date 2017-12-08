2579786e0be12c54d2ae5524250125d86f227702
Caleb Black <caleb.paul.black@gmail.com>
2017-12-08 16:54:18 -0700

    make dataset class

--- day8/solve.js
+++ day8/solve.js
@@ -1,9 +1,14 @@
 const fs = require('fs');
+const DataSet = require('./dataset');
+
 fs.readFile('puzzle.txt','utf-8',(err,data)=>{
   if (err) throw err;
   let instructions = data.split('\n').filter(instruction=>instruction.length>0&&instruction.split(' ').length == 7).map(instruction=>{
     let parts = instruction.split(' ');
     return {var:parts[0],dir:parts[1]=='inc'?'+':'-',conditional:parts.slice(4)};
   });
-  console.log(instructions);
+  console.log(solve(instructions));
 });
+function solve(instructions){
+  var data = new DataSet
+}
