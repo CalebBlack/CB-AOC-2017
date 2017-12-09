b4d0dbf702d88bba671bd685447f2dd7e21f179a
Caleb Black <caleb.paul.black@gmail.com>
2017-12-08 17:02:58 -0700

    finish solving problem

--- day8/solve.js
+++ day8/solve.js
@@ -5,10 +5,20 @@ fs.readFile('puzzle.txt','utf-8',(err,data)=>{
   if (err) throw err;
   let instructions = data.split('\n').filter(instruction=>instruction.length>0&&instruction.split(' ').length == 7).map(instruction=>{
     let parts = instruction.split(' ');
-    return {var:parts[0],dir:parts[1]=='inc'?'+':'-',conditional:parts.slice(4)};
+    return {var:parts[0],dir:parts[1]=='inc'?1:-1,amount:parts[2],conditional:parts.slice(4)};
   });
   console.log(solve(instructions));
 });
 function solve(instructions){
-  var data = new DataSet
+  var data = new DataSet();
+  instructions.forEach(instruction=>{
+    if (eval(data.get(instruction.conditional[0])+' '+instruction.conditional[1]+' '+instruction.conditional[2])) {
+      data.set(instruction.var,data.get(instruction.var) + instruction.dir * instruction.amount);
+    }
+  });
+  var largest = null;
+  Object.entries(data.data).forEach(pair=>{
+    if (!largest || largest[1] < pair[1]) largest = pair;
+  });
+  return largest;
 }
