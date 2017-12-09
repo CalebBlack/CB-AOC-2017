a90b61a6a4b8d1ed6ce85dfdffe860c2c1aa344e
Caleb Black <caleb.paul.black@gmail.com>
2017-12-08 17:06:57 -0700

    move puzzle to day root

--- day8/part1/solve.js
+++ day8/part1/solve.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const DataSet = require('./dataset');
 
-fs.readFile('puzzle.txt','utf-8',(err,data)=>{
+fs.readFile('../puzzle.txt','utf-8',(err,data)=>{
   if (err) throw err;
   let instructions = data.split('\n').filter(instruction=>instruction.length>0&&instruction.split(' ').length == 7).map(instruction=>{
     let parts = instruction.split(' ');
